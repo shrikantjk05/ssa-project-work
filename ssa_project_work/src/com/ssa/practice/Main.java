4c6ab34979d25efb542a3c8a01904e4402b7afc6
Arjun Metre <arjunprojectexecute@gmail.com>
2023-07-02 19:58:26 +0530

    updated code

---
+++ ssa_project_work/src/com/ssa/practice/Main.java
@@ -0,0 +1,14 @@
+package com.ssa.practice;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Please enter integer number");
+		int x= sc.nextInt();
+		System.out.println("You have entered "+x);
+	}
+}
--- ssa_project_work/src/ssa_project_work/Main.java
+++
@@ -1,8 +0,0 @@
-package ssa_project_work;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome");
-	}
-}
