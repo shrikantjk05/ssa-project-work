419089d4ff9ac7bc3867ea47286356774fa54e27
shrikantjk05 <138294867+shrikantjk05@users.noreply.github.com>
2023-07-02 20:41:09 +0530

    added readme.txt
    added readme.txt

--- ssa_project_work/src/com/ssa/practice/Main.java
+++ ssa_project_work/src/com/ssa/practice/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Banking Application");
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter you Name : ");
 		String name = sc.nextLine();
-  		System.out.println("Enter Customer account number : ");
-    	String accountNumber = sc.nextLine();
+		System.out.println("Enter Customer account number : ");
+		String accountNumber = sc.nextLine();
 
 	}
 }
