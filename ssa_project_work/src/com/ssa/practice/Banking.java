44930f64ebe9b2f3aa99430064f2ceee31a6bf3a
shrikantjk05 <138294867+shrikantjk05@users.noreply.github.com>
2023-07-02 22:25:37 +0530

    Today target complete
    Today target complete

---
+++ ssa_project_work/src/com/ssa/practice/Banking.java
@@ -0,0 +1,10 @@
+package com.ssa.practice;
+
+public class Banking {
+	public void Deposit()
+	{
+		int a=1;
+		System.out.println("Deposit       Press A : "+a);
+	}
+
+}
--- ssa_project_work/src/com/ssa/practice/Main.java
+++ ssa_project_work/src/com/ssa/practice/Main.java
@@ -5,12 +5,52 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+	
 		System.out.println("Welcome to Banking Application");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter you Name : ");
 		String name = sc.nextLine();
+		System.out.println("Enter ID Number : ");
+		String Id = sc.nextLine();
 		System.out.println("Enter Customer account number : ");
 		String accountNumber = sc.nextLine();
+
+		String[] a = { "Deposit       Press A : ", "Withdrawl     Press B : ", 
+				       "Transfer      Press c : ","View Balance  Press D : ",
+				       "Exit          Press E : " };
+		
+		for (int i = 0; i <a.length; i++) {
+			System.out.println(a[i]);
+		}
+		/*
+		 * System.out.println("Deposit       Press A : ");
+		 * System.out.println("Withdrawl     Press B : ");
+		 * System.out.println("Transfer      Press c : ");
+		 * System.out.println("View Balance  Press D : ");
+		 * System.out.println("Exit          Press E : ");
+		 */
+
+		System.out.println("Press : ");
+		String press = sc.next();
+		switch (press) {
+		case "A":
+			Banking D=new Banking();
+			D.Deposit();
+			/* System.out.println("Deposit       Press A : "); */
+			break;
+		case "B":
+			System.out.println("Withdrawl     Press B : ");
+			break;
+		case "C":
+			System.out.println("Transfer      Press c : ");
+			break;
+		case "D":
+			System.out.println("View Balance  Press D : ");
+			break;
+		case "E":
+			System.out.println("Exit          Press E : ");
+			break;
+		}
 
 	}
 }
